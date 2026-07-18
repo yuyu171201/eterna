b477cd7bd1c9e43bf29faa2b962cc923e3d7c02c
Yusuke Sumitani <suyu171201@gmail.com>
2026-07-18 15:50:09 +0900

    feat : 攻撃処理の追加

---
+++ battle.py
@@ -0,0 +1,38 @@
+class Unit:
+
+    # 初期化
+    def __init__(self,name , HP, ATK):
+        self.name = name
+        self.HP = HP
+        self.ATK = ATK
+
+
+def atack(unit1, unit2):
+    print(f"{unit1.name} の攻撃!")
+    print(f"{unit2.name} に {unit1.ATK} のダメージ!")
+    print()
+
+    unit2.HP = unit2.HP - unit1.ATK
+
+
+def battle(unit1, unit2):
+    print("バトル開始!\n")
+
+    step = 0
+    while unit1.HP > 0 and unit2.HP > 0:
+        step += 1
+        if step % 2 == 1:
+            atack(unit1, unit2)
+        else:
+            atack(unit2, unit1)
+
+    if unit1.HP <= 0:
+        print(f"{unit1.name} は倒れた!")
+    if unit2.HP <= 0:
+        print(f"{unit2.name} は倒れた!")
+
+
+if __name__ == "__main__":
+    yusha = Unit("勇者", 20, 10)
+    slime = Unit("スライム", 20, 5)
+    battle(yusha, slime)
