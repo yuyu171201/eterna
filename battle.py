42616e06d793dffbf4a0b35de1fd257fdf2341ec
Yusuke Sumitani <suyu171201@gmail.com>
2026-07-23 14:58:34 +0900

    feat : 素早さ順で行動するようにした。

--- battle.py
+++ battle.py
@@ -1,10 +1,11 @@
 class Unit:
 
     # 初期化
-    def __init__(self,name , hp, atk):
+    def __init__(self,name , hp, atk, spd):
         self.name = name
         self.hp = hp
         self.atk = atk
+        self.spd = spd
 
     def take_damage(self, damage):
         self.hp = self.hp - damage
@@ -27,34 +28,40 @@ def attack(unit1, unit2):
     return event
 
 
+def select_attack_order(attacker, unit1, unit2):
+    if attacker == unit1:
+        return unit2
+    else:
+        return unit1
+
+
 def battle(unit1, unit2):
+    turn = 0
     logs = []
 
-    step = 0
-    while unit1.hp > 0 and unit2.hp > 0:
-        step += 1
-        if step % 2 == 1:
-            logs.append([attack(unit1, unit2), step])
-        else:
-            logs.append([attack(unit2, unit1), step])
+    if unit1.spd > unit2.spd:
+        steps = [unit1, unit2]
+    else:
+        steps = [unit2, unit1]
 
-
-    # 勝敗判定 ： 倒された側を返す
-    if unit1.hp <= 0:
-        return unit1, logs
-    if unit2.hp <= 0:
-        return unit2, logs
+    while True:
+        turn += 1
+        for step in steps:
+            enemy = select_attack_order(step, steps[0], steps[1])
+            logs.append([attack(step, enemy), turn])
+            if enemy.hp <= 0:
+                return enemy, logs
 
 
 if __name__ == "__main__":
-    yusha = Unit("勇者", 20, 10)
+    yusha = Unit("勇者", 20, 10, 100)
     yusha.show_status()
-    slime = Unit("スライム", 20, 5)
+    slime = Unit("スライム", 20, 5, 90)
     slime.show_status()
 
     print("バトル開始!\n")
 
     loser, logs = battle(yusha, slime)
     for log in logs:
-        print(f"step {log[1]}: {log[0][0]} は {log[0][1]} に {log[0][2]} のダメージを与えた!\n")
+        print(f"turn {log[1]}: {log[0][0]} は {log[0][1]} に {log[0][2]} のダメージを与えた!\n")
     print(f"{loser.name} は倒れた!")
