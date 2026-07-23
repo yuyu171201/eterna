66f48fe27e96eb79043f412081933b695d25d2f8
Yusuke Sumitani <suyu171201@gmail.com>
2026-07-23 15:28:06 +0900

    test : battleのロジックのテスト

---
+++ test_battle.py
@@ -0,0 +1,25 @@
+# import pytest
+
+from battle import Unit, battle
+
+def test_take_damage():
+    unit = Unit("勇者", 20, 10, 100)
+    unit.take_damage(5)
+    assert unit.hp == 15
+
+def test_not_minus_hp():
+    unit = Unit("勇者", 20, 10, 100)
+    unit.take_damage(100)
+    assert unit.hp == 0
+
+def test_fastest_unit():
+    unit1 = Unit("勇者", 20, 10, 100)
+    unit2 = Unit("スライム", 20, 5, 90)
+    loser, logs = battle(unit1, unit2)
+    assert logs[0][0][0] == "勇者"
+
+def test_loser():
+    unit1 = Unit("勇者", 20, 10, 100)
+    unit2 = Unit("スライム", 20, 5, 90)
+    loser, logs = battle(unit1, unit2)
+    assert loser.name == "スライム"
