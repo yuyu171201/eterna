eca09a3de658569915252b92dfb9f0552ad46ede
Yusuke Sumitani <suyu171201@gmail.com>
2026-08-21 05:14:30 +0900

    fix : idの除去漏れを除去

--- test_battle.py
+++ test_battle.py
@@ -11,7 +11,7 @@ from unit import Unit
 def test_set_id():
     unit1 = Unit("勇者", 20, 10, 100)
     unit2 = Unit("スライム", 20, 5, 90)
-    unit3 = Unit("魔王", 30, 15, 120, id=99)
+    unit3 = Unit("魔王", 30, 15, 120)
     assert unit1.id == 1
     assert unit2.id == 2
     assert unit3.id == 99
