58508b0436f5755d37c40996a2db58e0649b30b2
Yusuke Sumitani <suyu171201@gmail.com>
2026-09-08 18:00:22 +0900

    chore : 追加漏れを追加

--- battle.py
+++ battle.py
@@ -28,6 +28,8 @@ class BattleEltena:
         self.action_gauge = 0
         self.threshold = ACTION_COST
 
+        self.normal_attack = master.normal_attack
+
     def take_damage(self, damage):
         self.current_hp = max(self.current_hp - damage, 0)
 
